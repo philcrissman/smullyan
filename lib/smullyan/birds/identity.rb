05e754db63ce8fe57ca65ebe86e9aaacf66f45e7
Phil Crissman <phil.crissman@gmail.com>
2025-07-17 11:44:31 -0700

    Added derived version of I
    Added the derivation of I as SKK to the Identity combinator.

--- lib/smullyan/birds/identity.rb
+++ lib/smullyan/birds/identity.rb
@@ -1,13 +1,21 @@
 # frozen_string_literal: true
 
+require_relative 'starling'
+require_relative 'kestrel'
+
 module Smullyan
   module Birds
     # The Identity bird - returns its argument unchanged
     # Identity x = x
-    Identity = ->(x) { x }
+    
+    # I is SKK
+    I_derived = S.(K).(K)
+
+    I_direct = ->(x) { x }
 
     # Traditional combinator names
-    I = Identity      # I combinator
+    Identity = I_derived     # I combinator
+    I = Identity
     Idiot = Identity  # Alternative name from Smullyan's book
   end
 end
