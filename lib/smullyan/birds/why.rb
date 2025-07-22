09a73abdd424ab1a06253ec9250ea7ff7cbeae99
Phil Crissman <phil.crissman@gmail.com>
2025-07-22 14:46:53 -0700

    Remove the 'direct' version of Y; it won't work in Ruby,
    Since it would start to evaluate right away and cause a stack
    overflow. :(

--- lib/smullyan/birds/why.rb
+++ lib/smullyan/birds/why.rb
@@ -3,18 +3,12 @@
 module Smullyan
   module Birds
     # The Why bird (Y combinator) - fixed-point combinator
-    # Why f = f (Why f)
-    Y_direct = -> (f) {
+      # Why f = f (Why f)
+      Y_direct = -> (f) {
       ->(x) { f.(->(v) { x.(x).(v) }) }.(
         ->(x) { f.(->(v) { x.(x).(v) }) }
       )
     }
-
-    # This is supposedly the correct derivation, but it winds up
-    # as an infinite loop if we use it. :/ :? 
-    # TODO look into this later!
-    # S(K(SII))(S(S(KS)K)(K(SII)))
-    Y_derived = S.(K.(S.(I).(I))).(S.(S.(K.(S)).(K)).(K.(S.(I).(I))))
 
     # Traditional combinator names
     Why = Y_direct
